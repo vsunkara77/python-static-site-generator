53f6d985a526d079b6b967f45802dad71e7c737a
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-24 17:12:18 +0530

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -3,7 +3,6 @@ from ssg.site import Site
 
 def main(source = "content",dest = "dist"):
     config ={"source":source,"dest":dest}
-    site = Site(**config)
-    site.build()
+    site = Site(**config).build()
 
 typer.run(main())
