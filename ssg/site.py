a2139ffc1b120fd9e6eccbb38ecb88670291731f
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-24 17:05:55 +0530

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -5,11 +5,11 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self,path):
-        directory = self.dest/Path.relative_to(self.source)
+        directory = self.dest/path.relative_to(self.source)
         directory.mkdir(parents=True, exists_ok=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exists_ok=True)
         for path in self.source.rglob("*"):
-            if Path.is_dir():
+            if path.is_dir():
                 self.create_dir(path)
