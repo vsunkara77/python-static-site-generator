6f0f23df3e5da6f9451372cbac7bf297ebb37f31
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 16:29:44 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,3 +9,7 @@ class Parser:
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(self,path):
+        with open(path,"r") as file
+            return file.read()
