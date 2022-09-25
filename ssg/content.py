437b1956dd7b3e9cfe9ca9e40ef0318d7b08bdb1
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 21:40:03 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -5,3 +5,9 @@ from collections.abc import Mapping
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter,re.MULTILINE)
+
+    @classmethod
+    def load(cls,string):
+        _,fm,content = cls.__regex.split(string,2)
+        metadata = load(fm,Loader = FullLoader)
+        return cls(metadata,content)
