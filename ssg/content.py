45e64282eeb60007426194fd548068bc5adebd30
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 21:48:42 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -12,7 +12,6 @@ class Content(Mapping):
         metadata = load(fm,Loader = FullLoader)
         return cls(metadata,content)
 
-    def __init(self,metadata,content):
+    def __init__(self,metadata,content):
         data = metadata
         self.data = {"content":content}
-        
