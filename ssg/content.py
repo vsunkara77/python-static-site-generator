330aa059e7688d9e57677abd5ba1ed87c037adb4
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 21:24:38 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -3,6 +3,6 @@ from yaml import load
 from yaml import FullLoader
 from collections.abc import Mapping
 
-class Content(mapping):
+class Content(Mapping):
     __delimeter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter,re.MULTILINE)
