d31a229529ff94fe430212a0f7c36e483d4f225c
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 15:57:47 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,10 +1,9 @@
 from typing import List
 from pathlib import path
 
-Class Parser:
+class Parser:
     extensions: list[str] = []
 
     def valid_extension(self,extension):
         if extension in self.extensions:
             pass
-            
