8ce97380b33beb5f51fdcb600bcfa6462cdaa4a3
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 17:06:41 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -17,4 +17,4 @@ class Parser:
     def write(self,path,dest,content,ext = ".html"):
         full_path = dest/path.with_suffix(ext).name
         with open(full_path,"w") as file:
-            return file.write()
+            file.write(content)
