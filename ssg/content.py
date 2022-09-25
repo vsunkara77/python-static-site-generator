6e9810c474a24fb527f5f4ac5dec1ed1d6c7de8f
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 22:07:05 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -23,3 +23,7 @@ class Content(Mapping):
     @property
     def type(self):
         return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self,type):
+        self.data["type"] = type
