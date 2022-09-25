b8bc0372afaf2a0cabe01a6e3e3ec39c48d1601f
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 17:25:23 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import List
 from pathlib import Path
 
@@ -18,3 +19,6 @@ class Parser:
         full_path = dest/path.with_suffix(ext).name
         with open(full_path,"w") as file:
             file.write(content)
+
+    def copy(self,path,source,dest):
+        shutil.copy2(path,dest/path.relative_to(source))
