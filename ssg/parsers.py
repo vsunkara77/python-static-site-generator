e8c43fb7c4a35a4f3059a776a6b230952b5c2298
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 16:12:53 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,6 +7,5 @@ class Parser:
     def valid_extension(self,extension):
         return extension in self.extensions
 
-    def parse(self, path:Path, source:Path, dest:Path)
+    def parse(self, path: Path, source: Path, dest: Path)
         raise NotImplementedError
-        
