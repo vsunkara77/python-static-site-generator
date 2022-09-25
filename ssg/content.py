57b10823b6c029acdff8d46fc8a46913746051df
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-25 21:20:09 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -2,3 +2,8 @@ import re
 from yaml import load
 from yaml import FullLoader
 from collections.abc import mapping
+
+class Content(mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter,re.MULTILINE)
+    
