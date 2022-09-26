ccae60d3333f738d97f07188b2c9d1409ca1a7a6
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-26 07:46:30 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -38,3 +38,12 @@ class MarkdownParser(Parser):
 
     def parse(self, path, source, dest):
             content = Content.load(self.read(path))
+            html = markdown(content.body)
+            self.write(path,dest,html)
+            sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+            content = Content.load(self.read(path))
