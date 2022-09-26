046ab2fff8cb957c0e2f91c7f315752ce9c92949
vsunkara77 <muralikumar.sunkara@gmail.com>
2022-09-26 07:52:25 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -47,3 +47,6 @@ class ReStructuredTextParser(Parser):
 
     def parse(self, path, source, dest):
             content = Content.load(self.read(path))
+            html = publish_parts(content.body, writer_name = "html5")
+            self.write(path,dest,html["html_body"])
+            sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
